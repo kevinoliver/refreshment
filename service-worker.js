9c30ac71cf61d88a9503514e7b030097a2731435
Kevin Oliver <kevinoliver@gmail.com>
2023-03-09 08:56:58 -0800

    fix badge state

--- service-worker.js
+++ service-worker.js
@@ -2,37 +2,33 @@
 
 const REFRESH_PREFIX = "refresh_tab_id_"
 
-chrome.runtime.onInstalled.addListener(() => {
-  // todo: this isn't working right. 
-  // it shows as off after the page reloads
-  // not sure where it gets from after the reload as this does not fire again
-  // and the other place we set badge text is in an event handler for onClicked.
-  // looks like this is set globally and when the page refreshes this is lost.
-  console.log("runtime installed. setting badge text to 'off'")
-  chrome.action.setBadgeText({
-    text: "off",
+function alarmNameForTabId(tabId) {
+  return REFRESH_PREFIX + tabId
+}
+
+async function setBadgeText(tabId, isRefreshing) {
+  // console.log("setting badge text to " + isRefreshing)
+  return chrome.action.setBadgeText({
+    tabId: tabId,
+    text: isRefreshing ? "on" : ""
   });
-});
+}
 
 chrome.action.onClicked.addListener(async (tab) => {
-  // Retrieve the action badge to check the current statge of the extension
+  // Retrieve the action badge to check the current state of the extension
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  const nextState = prevState === 'off' ? 'on' : 'off'
+  const isRefreshing = prevState === ""
   
-  // Set the action badge to the next state
-  await chrome.action.setBadgeText({
-    tabId: tab.id,
-    text: nextState,
-  });
+  setBadgeText(tab.id, isRefreshing)
 
-  if (nextState === 'on') {
+  if (isRefreshing) {
     chrome.alarms.create(
-      REFRESH_PREFIX + tab.id,
+      alarmNameForTabId(tab.id),
       { periodInMinutes: 1 }, // todo: set this to 5 
     );
   } else {
     chrome.alarms.clear(
-      REFRESH_PREFIX + tab.id
+      alarmNameForTabId(tab.id)
     );
   }
 });
@@ -44,4 +40,11 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   const stripped = alarm.name.slice(REFRESH_PREFIX.length)
   const tabId = Number(stripped) 
   chrome.tabs.reload(tabId)
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status == 'complete') {
+    const alarm = await chrome.alarms.get(alarmNameForTabId(tabId))
+    setBadgeText(tabId, alarm !== undefined)
+  }
 });
